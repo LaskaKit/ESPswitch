da2005386e08662f90efec5cd361a20f616a99e6
Jaroslav Burian <jaroslav.burian@espressif.com>
2024-09-09 00:22:32 +0200

    Add prusa heater example

---
+++ SW/Prusa_cup_heater/Prusa_cup_heater.ino
@@ -0,0 +1,84 @@
+/* Test code for LaskaKit ESPswitch
+ * 
+ * Board:   LaskaKit ESPswitch (ESP32C3 Dev Module) https://www.laskakit.cz/laskakit-esp32-devkit/
+ * 
+ * Library: by Adafruit         https://github.com/adafruit/Adafruit_NeoPixel
+ *          by Miles Burton     https://github.com/milesburton/Arduino-Temperature-Control-Library
+ *          by Paul Stoffregen  https://github.com/PaulStoffregen/OneWire
+ *
+ * Email:podpora@laskakit.cz
+ * Web:laskakit.cz
+ */
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <Adafruit_NeoPixel.h>
+
+#define TEMP_BOTTOM 55
+#define TEMP_TOP    65
+
+#define DS18B20_PIN 3     // DS18B20 GPIO on Laskakit ESPswitch board
+#define LED_PIN     8     // LED GPIO on Laskakit ESPswitch board
+#define CH0_PIN     0     // Channel 0 GPIO on Laskakit ESPswitch board (not used in this example)
+#define CH1_PIN     1     // Channel 1 GPIO on Laskakit ESPswitch board (not used in this example)
+#define CH2_PIN     4     // Channel 2 GPIO on Laskakit ESPswitch board (not used in this example)
+#define CH3_PIN     5     // Channel 3 GPIO on Laskakit ESPswitch board
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(1, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+OneWire oneWire(DS18B20_PIN);         // Setup a oneWire instance to communicate with any OneWire devices
+DallasTemperature dallas(&oneWire);   // Pass our oneWire reference to Dallas Temperature sensor 
+
+float get_temp()
+{
+  dallas.requestTemperatures(); 
+  float tempC = dallas.getTempCByIndex(0);
+
+  if (tempC != DEVICE_DISCONNECTED_C)
+  {
+    Serial.print("Temperature is: ");
+    Serial.println(tempC);
+  }
+  else
+  {
+    Serial.println("Error: Could not read temperature data");
+  }
+
+  Serial.print(tempC);
+  Serial.println("ºC");
+  return tempC;
+}
+
+void regulate_heater(float temp_bottom, float temp_top) {
+  float temp = get_temp();
+  if (temp < temp_bottom) {
+    ledcWrite(CH3_PIN, 255);
+  } else if (temp > temp_top) {
+    ledcWrite(CH3_PIN, 0);
+  }
+}
+
+void control_led() {
+  if (ledcRead(CH3_PIN) > 0) {
+    pixels.setPixelColor(0, pixels.Color(255, 0, 0));
+  } else {
+    pixels.setPixelColor(0, pixels.Color(0, 255, 0));
+  }
+  pixels.show();
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  ledcAttach(CH3_PIN, 200, 8); // Attach channel 3 to the GPIO pin to control the heater
+  
+  dallas.begin();  // Start the DS18B20 sensor
+
+  pixels.begin();
+  pixels.setBrightness(10);
+}
+
+void loop() {
+  regulate_heater(TEMP_BOTTOM, TEMP_TOP);
+  control_led();
+}
