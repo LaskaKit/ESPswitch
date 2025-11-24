b1599e8677a0552f526370994d429a533609c072
Konstantin Láska <konstantin.laska@icloud.com>
2025-11-24 09:59:54 +0100

    Update Test_ESPSwitch.ino

--- SW/Test_ESPSwitch/Test_ESPSwitch.ino
+++ SW/Test_ESPSwitch/Test_ESPSwitch.ino
@@ -14,6 +14,7 @@
 #include <DallasTemperature.h>
 #include <Adafruit_NeoPixel.h>
 
+// pro verze do v1.2
 #define DS18B20_PIN 3     // DS18B20 GPIO on Laskakit ESPswitch board
 #define LED_PIN     8     // LED GPIO on Laskakit ESPswitch board
 #define CH0_PIN     0     // Channel 0 GPIO on Laskakit ESPswitch board (not used in this example)
@@ -30,8 +31,11 @@ DallasTemperature dallas(&oneWire);   // Pass our oneWire reference to Dallas Te
 void setup() {
   Serial.begin(115200);
 
-  ledcAttach(CH3_PIN, 200, 8);  // Start the DS18B20 sensor
-  
+  ledcAttach(CH0_PIN, 200, 8);
+  ledcAttach(CH1_PIN, 200, 8);
+  ledcAttach(CH2_PIN, 200, 8);
+  ledcAttach(CH3_PIN, 200, 8);
+
   dallas.begin();  // Start the DS18B20 sensor
 
   pixels.begin();
@@ -42,36 +46,90 @@ void loop() {
 
   dallas.requestTemperatures(); 
   float temperatureC = dallas.getTempCByIndex(0);
-  Serial.print(temperatureC);
-  Serial.println("ºC");
+  if (temperatureC == -127) {
+    Serial.println("No sensor connected! Connect DS18B20 or check wiring");
+  } else {
+    Serial.print(temperatureC);
+    Serial.println("ºC");
+  }  
 
+// Test Channel 0, RED LED when active
+  Serial.println("Test channel 0");
   pixels.setPixelColor(0, pixels.Color(255, 0, 0)); // R, G, B
   pixels.show();
-  delay(1000);
 
-  pixels.setPixelColor(0, pixels.Color(0, 255, 0)); // R, G, B
-  pixels.show();
-  delay(1000);
-
-  pixels.setPixelColor(0, pixels.Color(0, 0, 255)); // R, G, B
-  pixels.show();
-  delay(1000);
-
-  pixels.setPixelColor(0, pixels.Color(255, 255, 255)); // R, G, B
-  pixels.show();
 
   for(int dutyCycle = 0; dutyCycle <= 255; dutyCycle++){   
+    // changing the motor rpm with with PWM
+    ledcWrite(CH0_PIN, dutyCycle);
+    delay(15);
+  }
+  delay(1000);
+
+  for(int dutyCycle = 255; dutyCycle >= 0; dutyCycle--){
     // changing the LED brightness with PWM
+    ledcWrite(CH0_PIN, dutyCycle);
+    delay(15);
+  }
+  delay(500);
+
+// Test Channel 1, GREEN LED when active
+  Serial.println("Test channel 1");
+  pixels.setPixelColor(0, pixels.Color(0, 255, 0)); // R, G, B
+  pixels.show();
+
+
+  for(int dutyCycle = 0; dutyCycle <= 255; dutyCycle++){   
+    // changing the motor rpm with with PWM
+    ledcWrite(CH1_PIN, dutyCycle);
+    delay(15);
+  }
+  delay(1000);
+
+  for(int dutyCycle = 255; dutyCycle >= 0; dutyCycle--){
+    // changing the LED brightness with PWM
+    ledcWrite(CH1_PIN, dutyCycle);
+    delay(15);
+  }
+  delay(500);
+
+// Test Channel 2, BLUE LED when active
+  Serial.println("Test channel 2");
+  pixels.setPixelColor(0, pixels.Color(0, 0, 255)); // R, G, B
+  pixels.show();
+
+
+  for(int dutyCycle = 0; dutyCycle <= 255; dutyCycle++){   
+    // changing the motor rpm with with PWM
+    ledcWrite(CH2_PIN, dutyCycle);
+    delay(15);
+  }
+  delay(1000);
+
+  for(int dutyCycle = 255; dutyCycle >= 0; dutyCycle--){
+    // changing the LED brightness with PWM
+    ledcWrite(CH2_PIN, dutyCycle);
+    delay(15);
+  }
+  delay(500);
+
+// Test Channel 3, VIOLET LED when active
+  Serial.println("Test channel 3");
+  pixels.setPixelColor(0, pixels.Color(255, 0, 255)); // R, G, B
+  pixels.show();
+
+
+  for(int dutyCycle = 0; dutyCycle <= 255; dutyCycle++){   
+    // changing the motor rpm with with PWM
     ledcWrite(CH3_PIN, dutyCycle);
     delay(15);
   }
+  delay(1000);
 
-  // decrease the LED brightness
   for(int dutyCycle = 255; dutyCycle >= 0; dutyCycle--){
     // changing the LED brightness with PWM
     ledcWrite(CH3_PIN, dutyCycle);
     delay(15);
   }
-  
-  delay(1000);
+  delay(500);
 }
